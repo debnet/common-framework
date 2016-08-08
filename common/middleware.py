53c1306627c097aabcd973baa23e8afe58dbca4b
Marc <marc.debureaux@sa-cim.fr>
2016-08-08 09:56:38 +0200

    Mise à jour middleware (Django 1.10)

--- common/middleware.py
+++ common/middleware.py
@@ -112,6 +112,20 @@ class ServiceUsageMiddleware:
     """
     Middleware des statistiques d'utilisation des services HTTP
     """
+
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = None
+        if hasattr(self, 'process_request'):
+            response = self.process_request(request)
+        if not response:
+            response = self.get_response(request)
+        if hasattr(self, 'process_response'):
+            return self.process_response(request, response)
+        return response
+
     def process_response(self, request, response):
         if settings.IP_DETECTION:
             try:
