551d36a8addf05d91fbc4ad37a47a0eee46bb1be
maxence <maxence.courtens@sa-cim.fr>
2017-01-20 14:51:04 +0100

    Ajout du 'kill' sur la commande 'abort_query'

--- common/management/commands/abort_query.py
+++ common/management/commands/abort_query.py
@@ -16,9 +16,10 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('name', type=str, help=_("Nom de l'application"))
+        parser.add_argument('--kill', dest='kill', action='store_true', help=_("Tue la requête au lieu de l'annuler"))
         parser.add_argument('--using', dest='using', type=str, help=_("Nom de la base de donnée ciblée"))
         parser.add_argument('--timeout', dest='timeout', type=int, help=_("Timeout"))
 
-    def handle(self, name, using=None, timeout=None, **options):
-        count = abort_query(name, using=using, timeout=timeout)
+    def handle(self, name, kill=False, using=None, timeout=None, **options):
+        count = abort_query(name, kill=kill, using=using, timeout=timeout)
         logger.info(_("{} requête(s) supprimée(s).").format(count))
